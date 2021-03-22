5e52384e7d72dca67f469da4a49f69d2b275296b
daianne16 <daianneleal16@gmail.com>
2021-03-22 11:09:39 -0300

    Terceira parte

--- src/Main2.java
+++
@@ -1,9 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        String x = "q1+q2";
-        System.out.println(x.contains("q1"));
-        System.out.println(x.contains("q2"));
-        System.out.println(x.contains("q3"));
-
-    }
-}
